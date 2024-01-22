fca605fdb82ee36aa8d187d278811ae091e2af6b
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-22 14:53:58 -0300

    Revisões nas funções de banco de dados

--- Desafios_Obrigatorios/Aula_14/f_sql.py
+++ Desafios_Obrigatorios/Aula_14/f_sql.py
@@ -4,6 +4,7 @@ Importante >> a bibliotexa SQLAlchemy deve estar instalada!!!!!!
 
 '''
 
+import pandas as pd
 import sqlite3
 from sqlalchemy import create_engine
 import sys
@@ -15,23 +16,46 @@ def connect_to_db(db_name = "coderhouse_python_54360.db"):
 
 # Checa se a tabela já existe no banco de dados
 def check_table_in_db(table_name, db_conn):
+    if pd.io.sql.table_exists(table_name, db_conn):
+        return True
+    return False
+
+# Fecha a conexão com o banco de dados
+def close_db_connection(db_conn):
+    db_conn.close()
+
+# Retorna uma lista com os nomes das tabelas do banco de dados
+def get_table_names(db_conn):
     
-    return True
+    cursor = db_conn.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    table_names = [row[0] for row in cursor.fetchall()]
+
+    return table_names
 
 # Carrega o dataFrame no banco de dados
-def dataframe_to_db(my_dataframe, table_name, db_conn = "coderhouse_python_54360.db"):
-    if not check_table_in_db(table_name, db_conn):
-        my_dataframe.to_sql(table_name, db_conn, if_exists='replace', index=False)
-        print(f"Tabela {table_name} carregada com sucesso no {db_conn}.")
-        return True
-    elif input(f"A tabela {table_name} já existe no banco de dados {my_dataframe}.\n _
-               Gostaria de sobrescrever a tabela? Responda Sim ou Nao: ").lower() == "s":
-        print(f"Tabela {table_name} carregada com sucesso no {db_conn}.")
-        return True
-    print(f"Sem alterações no {db_conn}.")
-    return False
-        
+def dataframe_to_db(my_dataframe, table_name, db_name = "coderhouse_python_54360.db"):
+    
+    db_conn = connect_to_db(db_name)
+    db_name = db_conn.execute("PRAGMA database_list;").fetchall()[0][2]
+    db_name = db_name[db_name.rfind("\\")+1:]
 
-
-# Fechar a conexão com o banco de dados
-conn.close()
+    if check_table_in_db(table_name, db_conn):
+    
+        print(f"A tabela {table_name} já existe no banco de dados {db_name}.")
+        ck_overwrite = input(f"Gostaria de sobrescrever a tabela? Responda Sim ou Nao: ").lower()
+    
+        if ck_overwrite == "s":
+            my_dataframe.to_sql(table_name, db_conn, if_exists='replace', index=False)
+            print(f"Tabela {table_name} carregada com sucesso no {db_name}.")
+            close_db_connection(db_conn)
+            return True
+        else:
+            print(f"Sem alterações no {db_name}.")
+            close_db_connection(db_conn)
+            return False
+    
+    print(f"Tabela {table_name} carregada com sucesso no {db_name}.")
+    close_db_connection(db_conn)
+    return True
