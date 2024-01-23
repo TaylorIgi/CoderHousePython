3cfdb2027f3bbcb76aa92b87925e0008dbb59ef2
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-23 13:42:37 -0300

    Funções para tratamento de APIs e tests para as funções

---
+++ Desafios_Obrigatorios/Aula_14/functions_api.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import requests
+import alerta_aula_04 as warning
+
+# Faz a requisição dos dados da API
+def api_get_data(url_path):
+    response = requests.get(url_path)
+    return response
+
+# Checa se o retorno da API foi bem sucedido
+def check_api_get_data(response_sts_code):
+    if response_sts_code in range(200, 300):
+        return True
+    return False
+
+# Retorna um dataframe com os dados da API
+def from_api_to_dataframe(my_object):
+    return pd.DataFrame(my_object.json())
+
+# Retorna um dataframe com a lista de APIs
+def get_api_list_from_xls(excel_path="DE_PARA_API_URL.xlsx", my_sheet_name="de_para"):
+    return pd.read_excel(excel_path, sheet_name=my_sheet_name)
+
+# "Printa" a lista de APIs disponíveis 
+def print_api_list(api_list_df):
+    if len(api_list_df) == 0:
+        print("Lista vazia!")
+        return False
+    print("-"*40)
+    for my_row in range(0, len(api_list_df)):
+        print(f"{api_list_df.index[my_row]} >> {api_list_df.loc[my_row, "API"]}")
+    print("-"*40)
+    return True
+
+# Retorna o índice da API escolhida (-1 se der erro na API)
+def choose_api(api_list_df):
+    print_api_list(api_list_df)
+    choosen_table_index = int(input(f"Digite o número da tabela: "))
+    if not check_api_get_data(api_get_data(api_list_df.loc[choosen_table_index, "URL"]).status_code):
+        warning.alerta(3, api_list_df.loc[choosen_table_index, "API"], "Request GET URL")
+        return -1
+    return choosen_table_index
+
+# Retorna um dataframe com os dados da API escolhida
+def get_dataframe(api_list_df, choosen_table_index):
+    if choosen_table_index == -1:
+        return pd.DataFrame()
+    choosen_table_url_data = api_get_data(api_list_df.loc[choosen_table_index, "URL"])
+    return from_api_to_dataframe(choosen_table_url_data)
--- Desafios_Obrigatorios/Aula_14/tests.py
+++ Desafios_Obrigatorios/Aula_14/tests.py
@@ -2,39 +2,49 @@ import classes
 import pandas as pd
 import sqlite3
 import functions_sql
+import functions_api
+import utils
 
-# Dataframe para teste 1
-dados1 = {
-    'Nome': ['Alice', 'Bob', 'Charlie', 'David'],
-    'Idade': [25, 30, 22, 35],
-    'Cidade': ['São Paulo', 'Rio de Janeiro', 'Curitiba', 'Porto Alegre']
-}
-df1 = pd.DataFrame(dados1)
+api_list_df = functions_api.get_api_list_from_xls()
+table_index = functions_api.choose_api(api_list_df)
+df = functions_api.get_dataframe(api_list_df, table_index)
 
-# Dataframe para teste 2
-dados2 = {
-    'Produto': ['Maçã', 'Arroz', 'Feijão', 'Frango', 'Salada'],
-    'Categoria': ['Fruta', 'Cereal', 'Legume', 'Proteína', 'Vegetal'],
-    'Calorias': [52, 130, 230, 165, 50],
-    'Preço': [2.5, 3.0, 4.5, 8.0, 2.0]
-}
-df2 = pd.DataFrame(dados2)
+print(df.head())
 
-teste = functions_sql.connect_to_db("DB_TESTE.db")
 
-# Inclui df1 no db e nas tabelas da instancia de mydb
-functions_sql.add_df_to_db(teste, df1, "tb_df1")
 
-# Inclui df2 no db e nas tabelas da instancia de mydb
-functions_sql.add_df_to_db(teste, df2, "tb_df2")
+# # Dataframe para teste 1
+# dados1 = {
+#     'Nome': ['Alice', 'Bob', 'Charlie', 'David'],
+#     'Idade': [25, 30, 22, 35],
+#     'Cidade': ['São Paulo', 'Rio de Janeiro', 'Curitiba', 'Porto Alegre']
+# }
+# df1 = pd.DataFrame(dados1)
 
-db_tables = functions_sql.get_db_tables(teste.db_conn)
+# # Dataframe para teste 2
+# dados2 = {
+#     'Produto': ['Maçã', 'Arroz', 'Feijão', 'Frango', 'Salada'],
+#     'Categoria': ['Fruta', 'Cereal', 'Legume', 'Proteína', 'Vegetal'],
+#     'Calorias': [52, 130, 230, 165, 50],
+#     'Preço': [2.5, 3.0, 4.5, 8.0, 2.0]
+# }
+# df2 = pd.DataFrame(dados2)
 
-print("-"*40)
-print("Tabelas no DB:")
-for table in db_tables:
-    print(f"Tabela >> {table}")
+# teste = functions_sql.connect_to_db("DB_TESTE.db")
 
-print("-"*40)
-print("Tabelas no objeto:")
-teste.list_tables()
+# # Inclui df1 no db e nas tabelas da instancia de mydb
+# functions_sql.add_df_to_db(teste, df1, "tb_df1")
+
+# # Inclui df2 no db e nas tabelas da instancia de mydb
+# functions_sql.add_df_to_db(teste, df2, "tb_df2")
+
+# db_tables = functions_sql.get_db_tables(teste.db_conn)
+
+# print("-"*40)
+# print("Tabelas no DB:")
+# for table in db_tables:
+#     print(f"Tabela >> {table}")
+
+# print("-"*40)
+# print("Tabelas no objeto:")
+# teste.list_tables()
