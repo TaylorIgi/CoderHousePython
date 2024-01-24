331e15462fefdade24aeb57b774c87000b52f232
TaylorIgi <taylor.igi.1@gmail.com>
2024-01-24 18:17:38 -0300

    Arquivos do desafio da aula 14

---
+++ Desafios_Complementares/Aula_16/functions_sql.py
@@ -0,0 +1,31 @@
+import sqlite3
+import classes
+import os
+
+# Retorna o nome das tabelas de um database
+def get_db_tables(conn):
+    
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    table_names = [row[0] for row in cursor.fetchall()]
+
+    return table_names
+
+# Conecta no database enviado e retorna um objeto MyDatabase
+def connect_to_db(database_name):
+    conn = sqlite3.connect(database_name)
+    return classes.MyDatabase(database_name, conn)
+
+# Dropa a base de dados, se existir
+def drop_db(database_name):
+    if os.path.exists(database_name):
+        conn = sqlite3.connect(database_name)
+        cursor = conn.cursor()
+        conn.close()
+        os.remove(database_name)
+
+# Adiciona um dataframe no database do object_mydatabase e inclui o nome da tabela no mesmo objeto
+def add_df_to_db(object_mydatabase, dataframe, database_table_name):
+    dataframe.to_sql(database_table_name, object_mydatabase.db_conn, if_exists="replace", index=False)
+    object_mydatabase.add_table(database_table_name)
---
+++ Desafios_Complementares/Aula_16/transform_api.py
@@ -0,0 +1,196 @@
+import pandas as pd
+import numpy as np
+
+def to_int(my_dataframe, int_columns):
+    for col in int_columns:
+        my_dataframe[col] = my_dataframe[col].astype(int)
+    return my_dataframe
+
+def to_float(my_dataframe, float_columns):
+    for col in float_columns:
+        my_dataframe[col] = my_dataframe[col].astype(float)
+    return my_dataframe
+
+def to_date(my_dataframe, date_columns):
+    for col in date_columns:
+        my_dataframe[col] = pd.to_datetime(my_dataframe[col])
+    return my_dataframe
+
+def transform_bancos(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0, subset=["ispb", "code"])
+    
+    subset_int = ["code"]
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+    
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = []
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+
+    return clean_dataframe
+
+def transform_corretoras(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0, subset=["cnpj", "codigo_cvm"])
+    
+    subset_int = ["codigo_cvm"]
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    clean_dataframe["valor_patrimonio_liquido"].replace(np.nan, 0, inplace=True)
+    
+    '''
+    Analisar pq não foi possível converter do mesmo modo dos inteiros!!!!!!!!!!!!!
+    '''
+    # subset_float = ["valor_pratrimonio_liquido"]
+    # clean_dataframe = to_float(clean_dataframe, subset_float)
+    clean_dataframe["valor_patrimonio_liquido"] = list(map(float, clean_dataframe["valor_patrimonio_liquido"]))
+    
+    subset_date = ["data_patrimonio_liquido", "data_inicio_situacao", "data_registro"]
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+        
+    return clean_dataframe
+
+def transform_cidade_uf(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0, subset=["nome", "id"])
+
+    subset_int = ["id"]
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = []
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+
+    return clean_dataframe
+
+def transform_clima(my_dataframe):
+
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0, subset=["codigo_icao", "atualizado_em"])
+
+    subset_int = ["umidade", "pressao_atmosferica", "vento", "direcao_vento", "temp"]
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = ["atualizado_em"]
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+    
+    return clean_dataframe
+
+def transform_feriados(my_dataframe):
+
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0)
+
+    subset_int = []
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = ["date"]
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+    
+    return clean_dataframe
+
+def transform_uf_regiao(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+
+    clean_dataframe["regiao_id"] = clean_dataframe["regiao"].apply(lambda x: x["id"])
+    clean_dataframe["regiao_sigla"] = clean_dataframe["regiao"].apply(lambda x: x["sigla"])
+    clean_dataframe["regiao_nome"] = clean_dataframe["regiao"].apply(lambda x: x["nome"])
+    clean_dataframe = clean_dataframe.drop(columns=["regiao"])
+
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0)
+
+    subset_int = ["id", "regiao_id"]
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = []
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+    
+    return clean_dataframe
+
+def transform_ncm(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0, subset=["codigo", "data_inicio", "numero_ato", "ano_ato"])
+    clean_dataframe = clean_dataframe.drop(columns=["data_fim"])
+
+    subset_int = ["numero_ato", "ano_ato"]
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = ["data_inicio"]
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+    
+    return clean_dataframe
+
+def transform_pix(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    my_subset = ["ispb", "nome", "modalidade_participacao", "tipo_participacao", "inicio_operacao"]
+    clean_dataframe = clean_dataframe.dropna(axis=0, subset=my_subset)
+
+    subset_int = []
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = []
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = ["inicio_operacao"]
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+    
+    return clean_dataframe
+
+def transform_taxas(my_dataframe):
+    
+    clean_dataframe = my_dataframe.copy()
+    clean_dataframe.replace("", np.nan, inplace=True)
+    clean_dataframe.drop_duplicates()
+    clean_dataframe = clean_dataframe.dropna(axis=0)
+
+    clean_dataframe["valor_percentual"] = clean_dataframe["valor"].apply(lambda x: x)
+    clean_dataframe["valor"] = clean_dataframe["valor"].apply(lambda x: x/100)
+
+    subset_int = []
+    clean_dataframe = to_int(clean_dataframe, subset_int)
+
+    subset_float = ["valor_percentual", "valor"]
+    clean_dataframe = to_float(clean_dataframe, subset_float)
+
+    subset_date = []
+    clean_dataframe = to_date(clean_dataframe, subset_date)
+    
+    return clean_dataframe
